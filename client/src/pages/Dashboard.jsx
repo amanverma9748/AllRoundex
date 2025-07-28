ebf11f2e453ea30f81edf6dc53b8322f846a5843
unknown <amanverma9748@gmail.com>
2025-07-28 21:55:59 +0530

    Dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyCreationData } from '../assets/assets'
+import { Sparkle } from 'lucide-react'
 
 const Dashboard = () => {
+
+
+
     return (
-        <div>
-            <h1>Dashboard</h1>
+        <div className='h-full overflow-scroll p-6'>
+            <div className='flex justify-start gap-4 flex-wrap'>
+                {/* Total Creation Card */}
+                <div className='flex justify-between items-center w-72 p-4 px-6 bg-white rounded-xl border border-gray-200'>
+                    <div>
+                        <p>Total Creations</p>
+                        <h2>{creations.length}</h2>
+                    </div>
+                    <div>
+                        <Sparkle className='w-5 text-white' />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
