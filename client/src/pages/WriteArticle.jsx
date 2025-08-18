df652f8be9306bb69bef86051abb3e5cd047023f
unknown <amanverma9748@gmail.com>
2025-08-18 19:59:30 +0530

    Completed write article section

--- client/src/pages/WriteArticle.jsx
+++ client/src/pages/WriteArticle.jsx
@@ -12,10 +12,14 @@ const WriteArticle = () => {
     const [selectedLength, setSelectedLength] = useState(articleLength[0])
     const [input, setInput] = useState('')
 
+    const onSubmitHandler = async (e) => {
+        e.preventDefault();
+    }
+
     return (
         <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700'>
             {/*left col*/}
-            <form className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200' >
+            <form onSubmit={onSubmitHandler} className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200' >
                 <div className='flex items-center gap-3'>
                     <Sparkles className='w-6 text-[#4A7AFF]' />
                     <h1 className='text-xl font-semibold'>Article Configuration</h1>
@@ -40,8 +44,17 @@ const WriteArticle = () => {
 
             </form>
             {/*right col*/}
-            <div>
-
+            <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 max-h-[600px]'>
+                <div className='flex items-center gap-3'>
+                    <Edit className='w-5 h-5 text-[#4A7AFF]' />
+                    <h1 className='text-xl font-semibold'>Generated article</h1>
+                </div>
+                <div className='flex-1 flex justify-center items-center'>
+                    <div className='text-sm flex flex-col items-center gap-5 text-gray-400'>
+                        <Edit className='w-9 h-9' />
+                        <p>Enter a topic and click "Generate article" to get started</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
